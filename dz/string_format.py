d43f671cc4d6b160dff9b61f9dbe24c33d00488a
AL1EN <wipetmp1404@proton.me>
2023-11-22 21:11:54 +0500

    second commit

---
+++ dz/string_format.py
@@ -0,0 +1,51 @@
+# 1-primer
+#!1-jol
+"""
+str = input("Введите строку:")
+str = str.lower().capitalize()
+new_str = ""
+counter = 0
+for char in str:
+    if counter == (len(str)-1):
+        char = char.upper()
+    new_str += char
+    counter += 1
+print(new_str)
+"""
+#!2-jol
+"""
+str = input("Введите строку:")
+str = str.lower().capitalize()
+last_char_index = len(str)-1
+last_char = str[last_char_index].upper()
+new_str = str[0:last_char_index]+last_char
+print(new_str)
+"""
+
+#!2-primer
+"""
+str = input("Введите строку:")
+str=str.lstrip().rstrip()
+str_list = str.split(" ")
+str_list_len = len(str_list)
+print(str_list_len)
+"""
+#!3-primer
+"""
+password = input("Введите пароль:")
+first_char = password[0]
+pass_len = len(password)
+if pass_len >= 8 and first_char.upper() == password[0]:
+    print("Пароль принят")
+else:
+    print("Ваш пароль не принят")
+"""
+
+my_dict = {
+    "name": 'Timur',
+    "age": "25"
+}
+print("user name:{}\nuser age:{}".format(
+    my_dict.get("name"), my_dict.get("age")))
+p=my_dict.pop('name')
+print(my_dict)
--- lesson1/test.py
+++ lesson1/test.py
@@ -1,2 +1,18 @@
-value = input("input :")
-print(None if not value else value)
+# value = input("input :")
+# print(None if not value else value)
+
+# ! Loop
+# word = 'hello'
+# new_word=""
+# for char in word:
+#     if char == 'l':
+#         char = 's'
+
+#     new_word += char
+# print(new_word)
+
+# counter = 99
+# while (counter > 0):
+#     if counter % 5 == 0:
+#         print(counter)
+#     counter -=1
