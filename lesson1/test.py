618b330567d4729c4a33961f28b0a34ce4e44140
AL1EN <wipetmp1404@proton.me>
2023-11-30 20:48:16 +0500

    test

--- lesson1/test.py
+++ lesson1/test.py
@@ -69,3 +69,100 @@ print(result)
 """
 #!Функции 2-часть
 
+"""
+x = [1, 2, 3, 4, 5]
+
+def make_smth(function):
+    for el in x:
+        print(function(el))
+        
+make_smth(lambda a: a+10)
+"""
+
+# ? Рекурсия
+"""
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+print(factorial(5))
+"""
+
+#! Исключения
+"""
+try:
+    c = 10/0
+except:
+    c = 0
+    print('An exception occurred')
+
+print(c)
+"""
+
+"""
+def x():
+    a = 10
+
+
+x = True
+try:
+    # print(a)
+    # print(int('abc'))
+    for el in x:
+        print(x)
+except ValueError as error:
+    print(error)
+except NameError as error:
+    print(error)
+except TypeError as error:
+    print(error)
+"""
+
+"""
+def get_month(number):
+    months = {
+        1: "Jan",
+        2: "Feb",
+        3: "Mar",
+        4: "Apr",
+        5: "May",
+        6: "Jun",
+        7: "Jul",
+        8: "Aug",
+        9: "Sep",
+        10: "Oct",
+        11: "Nov",
+        12: "Dec",
+    }
+    try:
+        return months[number]
+    except KeyError as error:
+        print(error, ", use only numbers from 1 to 12")
+    except TypeError as error:
+        print(error, ", use only numbers from 1 to 12")
+
+
+month_name = get_month([1, 2, 3])
+print(month_name)
+"""
+
+"""
+def check(sequence):
+    if len(set(sequence)) == len(sequence):
+        return True
+    return False
+
+
+def check_sequence_unique(array):
+    try:
+        return check(array)
+    except TypeError as error:
+        print(error, "use only strings,lists or sets")
+
+
+s = 'abcdd'
+u = check_sequence_unique(s)
+print(u)
+"""
